17eae4cf08b24d4ba3464592cdcee348c824a890
fernando-mac <luizfsegato@gmail.com>
2024-04-25 19:39:07 -0400

    Foi adicionado Security com BD e PasswordEncoder e Roles - Final

--- src/main/java/com/ms/email/configs/security/WebSecurityConfig.java
+++ src/main/java/com/ms/email/configs/security/WebSecurityConfig.java
@@ -3,22 +3,15 @@ package com.ms.email.configs.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-//@EnableWebSecurity
 public class WebSecurityConfig {
 
     @Value("${app.auth.username}")
@@ -33,6 +26,8 @@ public class WebSecurityConfig {
         http
                 .authorizeHttpRequests((authorize) -> authorize
                         //.anyRequest().permitAll() //igual ao padrão NoAuth
+                        .requestMatchers(HttpMethod.GET, "/sending-email").hasAnyRole("ADMIN","USER")
+                        .requestMatchers(HttpMethod.POST, "/sending-email").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
@@ -64,8 +59,4 @@ public class WebSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-//        return authenticationConfiguration.getAuthenticationManager();
-//    }
 }
